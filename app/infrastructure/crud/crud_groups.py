93da77fe09fb99bff6ad548aa3abd3150eb84469
Pascal PHAM <pham.anhkhoapascal@gmail.com>
2025-04-21 11:28:39 +0200

    Use FastCRUD

---
+++ app/infrastructure/crud/crud_groups.py
@@ -0,0 +1,4 @@
+from fastcrud import FastCRUD
+from ..models.group import Group
+
+group_crud = FastCRUD(Group)
---
+++ app/infrastructure/crud/crud_sites.py
@@ -0,0 +1,4 @@
+from fastcrud import FastCRUD
+from ..models.site import Site
+
+site_crud = FastCRUD(Site)
